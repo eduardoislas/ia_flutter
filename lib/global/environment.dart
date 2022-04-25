109b5404b343ea39da1694fa6f6c21b61fd701b1
eduardoislas <eislas22@gmail.com>
2022-04-25 14:31:06 -0700

    Corrección de Pruebas en server

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
 // Desarrollo
-// class Environment {
-//   static String apiUrl = Platform.isAndroid
-//       ? 'http://10.0.2.2:3000/api'
-//       : 'http://localhost:3000/api';
-
-//   static String socketUrl =
-//       Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
-// }
-
 class Environment {
-  static String apiUrl = 'asistenteapi.acislab.com/api';
+  static String apiUrl = Platform.isAndroid
+      ? 'http://10.0.2.2:3000/api'
+      : 'http://localhost:3000/api';
 
-  static String socketUrl = 'asistenteapi.acislab.com';
+  static String socketUrl =
+      Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
 }
+
+// class Environment {
+//   static String apiUrl = 'asistenteapi.acislab.com/api';
+
+//   static String socketUrl = 'asistenteapi.acislab.com';
+//}
